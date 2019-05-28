d58cf44ffc8887bf526adbfcbf9c274562fe70a8
Ana María Hincapié Berrio <50270277+ahincapieb@users.noreply.github.com>
2019-05-27 21:55:09 -0500

    Update q09.py

--- q09.py
+++ q09.py
@@ -2,6 +2,7 @@
 ## Construya una tabla que contenga _c1 y una lista
 ## separada por ':' de los valores de la columna _c2
 ## para el archivo tbl0.tsv
+import pandas as pd
 ## Leer archivo
 x = pd.read_csv('tbl0.tsv', sep = '\t')
 ## Construir tabla
