ecdd0d1ee877795c4a195c5c50624d447d55c8b5
Ana María Hincapié Berrio <50270277+ahincapieb@users.noreply.github.com>
2019-05-27 21:50:05 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -1,8 +1,9 @@
 ##
 ## Imprima la cantidad de registros por cada letra 
 ## de la columna _c1 de la tabla tbl0
+import pandas as pd
 ## Leer archivo
-##x = pd.read_csv('tbl0.tsv', sep = '\t')
+x = pd.read_csv('tbl0.tsv', sep = '\t')
 
 ## Cantidad de registros por cada una de las letras
 x.groupby('_c1').count()['_c0']
