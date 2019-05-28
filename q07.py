f5291af5114224e81f2e48a696050267e725637e
Ana María <ahincapieb@unal.edu.co>
2019-05-27 19:55:32 -0500

    Update q07.py

--- q07.py
+++ q07.py
@@ -2,7 +2,7 @@
 ## Agregue una columna  con la suma de _c0 y _c2 a la tabla tbl0.tsv 
 ## Leer archivo
 x = pd.read_csv('tbl0.tsv', sep = '\t')
-## Agregar columna
+## Agregar columna con la suma
 x['suma_c0_c2'] = x['_c0'] + x['_c2']
 x.head()
 
