6929195bfb59821fb847146fc01655956ee116f2
Ana María Hincapié Berrio <50270277+ahincapieb@users.noreply.github.com>
2019-05-27 22:05:04 -0500

    Update q04.py

--- q04.py
+++ q04.py
@@ -3,6 +3,7 @@
 ## de la tabla tbl1 en mayusculas
 ## Lectura del archivo
 import pandas as pd
+import numpy as np
 x1 = pd.read_csv('tbl1.tsv', sep = '\t')
 ## Impresion valores unicos
 unicos = x1['_c4'].unique()
