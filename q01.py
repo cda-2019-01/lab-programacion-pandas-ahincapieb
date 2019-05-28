7ffe8a68b62f885a314c8cb73ff32520db062662
Ana María Hincapié Berrio <50270277+ahincapieb@users.noreply.github.com>
2019-05-27 21:41:02 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -3,7 +3,6 @@
 ## de la columna _c1 de la tabla tbl0
 ## Leer archivo
 x = pd.read_csv('tbl0.tsv', sep = '\t')
-x.head()
 
 ## Cantidad de registros por cada una de las letras
 x.groupby('_c1').count()['_c0']
