86cadcb68cdc2414e47b38a6d90b9f617d1c2b11
Ana María Hincapié Berrio <50270277+ahincapieb@users.noreply.github.com>
2019-05-27 21:56:03 -0500

    Update q11.py

--- q11.py
+++ q11.py
@@ -2,9 +2,9 @@
 ## Construya una tabla que contenga _c0 y una lista
 ## separada por ',' de los valores de la columna _c5a
 ## y _c5b (unidos por ':') de la tabla tbl2.tsv
+import pandas as pd
 ## Leer archivo
 x2 = pd.read_csv('tbl2.tsv', sep = '\t')
-x2.head()
 ## Construccion de tabla
 x2['_c5'] = x2['_c5a'] + ":" + x2['_c5b'].astype('str')
 x2temp = x2.groupby('_c0')['_c5'].apply(list)
